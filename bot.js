2b9cfa8a5103a42bc7523055da569f999e1511d6
HBR8 <38672560+HBR8@users.noreply.github.com>
2018-04-27 02:14:52 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,12 +2,12 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    console.log('I am ready!');
+    console.log('Milkyway bot is on');
 });
 
 client.on('message', message => {
     if (message.content === '!milkyway') {
-    	message.reply('pong');
+    	message.reply("I am on!");
   	}
 });
 
