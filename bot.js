bdc1aedf84404bfc32d53cedb22fcf361b157b48
HBR8 <38672560+HBR8@users.noreply.github.com>
2018-04-27 00:55:54 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,22 @@
 const Discord = require('discord.js');
-const Client = new Discord.Client();
+const client = new Discord.Client();
 
-client.on('ready' , ()=> {
-   console.log('I am ready!');
+client.on('ready', () => {
+    console.log('I am ready!');
 });
 
-client.on{'message', message=> {
-  if(message.content === '-Milkyway'){ message.replay('Test');
-}
+client.on('message', message => {
+    if (message.content === '-Milkyway') {
+    	message.reply('test');
+  	}
 });
 
 client.login(process.env.BOT_TOKEN);
+
+          
+          
+
+
+
+
+
