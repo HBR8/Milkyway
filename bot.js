9f181d8b66576c21b82491985403fe6711f0bf25
HBR8 <38672560+HBR8@users.noreply.github.com>
2018-04-27 02:45:34 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,25 +7,6 @@ client.on("ready", funciton(){
 	console.log("Milkyway bot is online!");
 });
 
-client.on("message", function(message){
 
-	if(message.author.equals(client.user)) return;
-
-	if(!message.content.startsWith(PREFIX)) return;
-
-	var args = message.content.substring(PREFIX.length).split(" ");
-
-	switch(args[0].toLowerCase()){
-
-		case "embed":
-		var embed = new Discord.RichEmbed()
-		.setDescription("Hello!");
-		message.channel.sendEmbed(embed);
-
-
-		default: 
-			message.channel.sendMessage("Invalid command");
-	}
-});
 
 client.login(process.env.BOT_TOKEN);
