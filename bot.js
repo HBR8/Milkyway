1bf3feb87be408c1a605bc94178da8c54afbde79
HBR8 <38672560+HBR8@users.noreply.github.com>
2018-04-27 02:35:54 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,32 @@
 const Discord = require('discord.js');
+
 const client = new Discord.Client();
+const PREFIX = "*Milkyway";
 
-client.on('ready', () => {
-    console.log('Milkyway bot is on');
-});
 
-client.on('message', message => {
-    if (message.content === '!milkyway') {
-    	message.reply("I am on!");
-  	}
-});
+client.on("message", function(message){
+
+	if(message.author.equals(client.user)) return;
+
+	if(!message.content.startswith(PREFIX)) return;
+
+	var args = message.content.substring(PREFIX.length).split(" ");
+
+	switch(args[0].toLowerCase()){
+
+		case "embed":
+		var embed = new Discord.RichEmbed()
+		.setDescription("Hello!");
+		message.channel.sendEmbed(embed);
+
+
+		default: 
+			message.channel.sendMessage("Invalid command");
+
+
+
+	}
+
+}
 
 client.login(process.env.BOT_TOKEN);
