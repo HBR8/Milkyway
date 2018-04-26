319c8cad47af108d5d10a3df92a4d0749f972224
HBR8 <38672560+HBR8@users.noreply.github.com>
2018-04-27 01:17:01 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === '-Milkyway') {
+    if (message.content === 'ping') {
     	message.reply('test');
   	}
 });
