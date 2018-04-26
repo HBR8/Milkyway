e1f108f2c5a1acd06608ef9fb47090d35fc04c64
HBR8 <38672560+HBR8@users.noreply.github.com>
2018-04-27 01:53:08 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ client.on('message', message => {
     if (message.content === '!milkyway') {
     	 "embed": {
         "image": {
-            "discreption": "I have no idea"
+            "url": "https://media.discordapp.net/attachments/438022387519193098/438049757915643934/image.jpg?width=473&height=473"
         }
     }
   	}
