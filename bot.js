7683f330b542ef8a4d06304d57232c211d245bcb
HBR8 <38672560+HBR8@users.noreply.github.com>
2018-04-27 01:44:58 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,28 +1,14 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const Client = new Discord.Client();
 
-client.on('ready', () => {
-    console.log('I am ready!');
+client.on('ready' , ()=> {
+   console.log('I am ready!');
 });
 
-client.on('message', message => {
-    if (message.content === '!Milkyway') {
-            "embed": {
-        "image": {
-            "url": "https://media.discordapp.net/attachments/438022387519193098/438049757915643934/image.jpg?width=473&height=473"
-        }
-    }
+client.on{'message', message=> {
+  if(message.content === '-Milkyway')
+  	{ message.replay('Test');
 }
-          
-});
-
+};
 
 client.login(process.env.BOT_TOKEN);
-
-          
-          
-
-
-
-
-
