5c4f1ada6016f99d7b53d572c93f7cf7df5c6f65
HBR8 <38672560+HBR8@users.noreply.github.com>
2018-04-27 03:25:55 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,10 +14,37 @@ client.on("message", function(message){
 	switch(args[0].toLowerCase()){
 
 		case "milkyway": 
-		var clans = new Discord.RichEmbed(
-			.addField("E A R T H","TagHere")
-			
-			);
+		
+		message.channel.send({embed: {
+    color: 3447003,
+    author: {
+      name: client.user.username,
+      icon_url: client.user.avatarURL
+    },
+    title: "This is an embed",
+    url: "http://google.com",
+    description: "This is a test embed to showcase what they look like and what they can do.",
+    fields: [{
+        name: "Fields",
+        value: "They can have different fields with small headlines."
+      },
+      {
+        name: "Masked links",
+        value: "You can put [masked links](http://google.com) inside of rich embeds."
+      },
+      {
+        name: "Markdown",
+        value: "You can put all the *usual* **__Markdown__** inside of them."
+      }
+    ],
+    timestamp: new Date(),
+    footer: {
+      icon_url: client.user.avatarURL,
+      text: "© Example"
+    }
+  }
+});
+
 		
 		break;
 
