9d832fdb2e990b0069eeae43c607b5435fa4d586
HBR8 <38672560+HBR8@users.noreply.github.com>
2018-04-27 03:18:02 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,7 +17,8 @@ client.on("message", function(message){
 		case "milkyway": 
 		message.channel.send({embed: {
  		 color: 3447003,
-  		description: "A very simple Embed!"	
+  		description: "A very simple Embed!"
+  	}
 
 		default:
 			message.channel.sendMessage("invalid command");	
