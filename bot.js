0ac8046d50dae905c44c53ee33314ab179c49254
HBR8 <38672560+HBR8@users.noreply.github.com>
2018-04-28 00:39:23 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -57,15 +57,14 @@ client.on("message", function(message){
       }
     ],
 
-
+  setImage:{
+    url:"https://royaleapi.com/static/img/badge/Twin_Peaks_02.png"
+}
     timestamp: new Date(),
     footer: {
       icon_url: client.user.avatarURL,
       text: "Bot By HBR"
-    }
-
-  .setImage("https://royaleapi.com/static/img/badge/Twin_Peaks_02.png")
-
+    },
   }
 });
 
