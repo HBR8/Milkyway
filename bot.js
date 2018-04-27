23f26f4e873a0f9a2256c6a4bdb6efd4d117bee5
HBR8 <38672560+HBR8@users.noreply.github.com>
2018-04-27 03:32:45 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,13 +15,35 @@ client.on("message", function(message){
 
 		case "milkyway": 
 		
-		var clans = new Discord.RichEmbed()
-
-		.addField("E A R T H","TAG")
-
-		message.channel.sendMessage(clans);
-
-		break;
+		message.channel.send({embed: {
+    color: FF0000,
+    author: {
+      name: client.user.username,
+      icon_url: client.user.avatarURL
+    },
+    title: "This is an embed",
+    url: "http://google.com",
+    description: "This is a test embed to showcase what they look like and what they can do.",
+    fields: [{
+        name: "Fields",
+        value: "They can have different fields with small headlines."
+      },
+      {
+        name: "Masked links",
+        value: "You can put [masked links](http://google.com) inside of rich embeds."
+      },
+      {
+        name: "Markdown",
+        value: "You can put all the *usual* **__Markdown__** inside of them."
+      }
+    ],
+    timestamp: new Date(),
+    footer: {
+      icon_url: client.user.avatarURL,
+      text: "© Example"
+    }
+  }
+});
 
 		default:
 			message.channel.sendMessage("invalid command");	
