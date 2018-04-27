4a6def17e0bb9008f6021a27b41c86932c884d60
HBR8 <38672560+HBR8@users.noreply.github.com>
2018-04-27 03:28:37 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,37 +15,12 @@ client.on("message", function(message){
 
 		case "milkyway": 
 		
-		message.channel.send({embed: {
-    color: 3447003,
-    author: {
-      name: client.user.username,
-      icon_url: client.user.avatarURL
-    },
-    title: "This is an embed",
-    url: "http://google.com",
-    description: "This is a test embed to showcase what they look like and what they can do.",
-    fields: [{
-        name: "Fields",
-        value: "They can have different fields with small headlines."
-      },
-      {
-        name: "Masked links",
-        value: "You can put [masked links](http://google.com) inside of rich embeds."
-      },
-      {
-        name: "Markdown",
-        value: "You can put all the *usual* **__Markdown__** inside of them."
-      }
-    ],
-    timestamp: new Date(),
-    footer: {
-      icon_url: client.user.avatarURL,
-      text: "© Example"
-    }
-  }
-});
+		var clans = new RichEmbed()
 
-		
+		.addField("E A R T H","TAG")
+
+		message.channel.sendMessage(clans);
+
 		break;
 
 		default:
