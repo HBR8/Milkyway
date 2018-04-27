f878b7697368e8dc2bdd142fc9bf3ad35f457a2d
HBR8 <38672560+HBR8@users.noreply.github.com>
2018-04-28 00:22:01 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,27 +27,33 @@ client.on("message", function(message){
     fields: [
       {
         name: ":comet:  E A R T H **(#9VQ8V8YC)**",
-        value: ":medal: 40K+     :crossed_swords:Clan Wars"
+        value: "  :medal: 40K+     :crossed_swords:Clan Wars",
+        url: "https://royaleapi.com/clan/family/milkyway/clan/9VQ8V8YC"
       },
       {
         name: ":comet:  N E P T U N E **(#P0JJ9RVJ)**",
-        value: ":medal: 32K+     :crossed_swords:Clan Wars"
+        value: "  :medal: 32K+     :crossed_swords:Clan Wars",
+        url: "https://royaleapi.com/clan/family/milkyway/clan/P0JJ9RVJ"
       },
       {
         name: ":comet:  S A T U R N **(#P2P9VR02)**",
-        value: ":medal: 40K+     :crossed_swords:Clan Wars"
+        value: "  :medal: 40K+     :crossed_swords:Clan Wars",
+        url: "https://royaleapi.com/clan/family/milkyway/clan/P2P9VR02"
       },
       {
         name: ":comet:  M A R S **(#9RURCVRR)**",
-        value: ":medal: 35K+     :crossed_swords:Clan Wars"
+        value: "  :medal: 35K+     :crossed_swords:Clan Wars",
+        url: "https://royaleapi.com/clan/family/milkyway/clan/9RURCVRR"
       },
       {
         name: ":comet:  P L A N E T - X **(#99RP2Y2V)**",
-        value: ":medal: 37K+     :crossed_swords:Clan Wars"
+        value: "  :medal: 37K+     :crossed_swords:Clan Wars",
+        url: "https://royaleapi.com/clan/family/milkyway/clan/99RP2Y2V"
       },
       {
         name: ":comet:  J U P I T E R **(#P829Q822)**",
-        value: ":medal: 40K+     :crossed_swords:Clan Wars"
+        value: "  :medal: 40K+     :crossed_swords:Clan Wars",
+        url: "https://royaleapi.com/clan/family/milkyway/clan/P829Q822"
       }
     ],
     timestamp: new Date(),
