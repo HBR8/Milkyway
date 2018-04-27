04d04989bff21aa63ddbbe1bde1f55125649aa1f
HBR8 <38672560+HBR8@users.noreply.github.com>
2018-04-27 05:28:18 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,55 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+const PREFIX = "*";
+
+client.on("message", function(message){
+
+	if(message.author.equals(client.user)) return;
+	if(!message.content.startsWith(PREFIX)) return;
+	var args = message.content.substring(PREFIX.length).split(" ");
+
+	switch(args[0].toLowerCase()){
+
+		case "milkyway": 
+		
+		message.channel.send({embed: {
+    color: 0xff0000,
+    author: {
+      name: client.user.username,
+      icon_url: client.user.avatarURL
+    },
+    title: "Milkyway Family",
+    url: "http://google.com",
+    description: "this is our clans, choose the best one for you and join it now!",
+    fields: [{
+        name: "EARTH",
+        value: "TAG"
+      },
+      {
+        name: "MARS",
+        value: "TAG"
+      },
+      {
+        name: "SATURN",
+        value: "TAG"
+      }
+    ],
+    timestamp: new Date(),
+    footer: {
+      icon_url: client.author.avatarURL,
+      text: "By HBR"
+    }
+  }
+});
+
+
+		break;
+
+		default:
+			message.channel.sendMessage("invalid command");	
+
+	}
+});		
+
+client.login(process.env.BOT_TOKEN);
