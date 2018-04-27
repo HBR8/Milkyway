16e43c95e69d400053ffddc6cdde2b7742f084de
HBR8 <38672560+HBR8@users.noreply.github.com>
2018-04-27 03:09:30 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,24 +1,27 @@
 const Discord = require('discord.js');
 
 const client = new Discord.Client();
-const PREFIX = "*";
+const PREFIX = "*milkyway";
 
 
 client.on("message", function(message){
+
+	if(message.author.equals(client.user)) return;
+
+	if(!message.content.startsWith(PREFIX)) return;
+
 	var args = message.content.substring(PREFIX.length).split(" ");
 
 	switch(args[0].toLowerCase()){
 
-		case "milkyway":
-		var embed = new Discord.RichEmbed()
+		default
+
+		var embed = new Discord.RichEmbed(data);
 		
 		.addField("test title","Test desc")
 
 		message.channel.sendEmbed(embed);
-		break;
-			
-	default: 
-		message.channel.sendMessage("Invalid command");
+		
 	}
 });
 
