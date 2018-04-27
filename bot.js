ad2d75b34008c37abe71e7f2dd8b47390ff45d0b
HBR8 <38672560+HBR8@users.noreply.github.com>
2018-04-27 03:44:55 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,7 +16,7 @@ client.on("message", function(message){
 		case "milkyway": 
 		
 		message.channel.send({embed: {
-    color: FF0000,
+    color: 'red',
     author: {
       name: client.user.username,
       icon_url: client.user.avatarURL
