5104c9dde70ace23ea70a3d629879879a6f5cef5
HBR8 <38672560+HBR8@users.noreply.github.com>
2018-04-27 04:28:42 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -39,7 +39,7 @@ client.on("message", function(message){
     ],
     timestamp: new Date(),
     footer: {
-      icon_url: client.author.avatarURL,
+      icon_url: message.author.avatarURL,
       text: "By HBR"
     }
   }
