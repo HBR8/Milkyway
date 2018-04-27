808368cdecae831effcc3b6b04f52bc754aea4ee
HBR8 <38672560+HBR8@users.noreply.github.com>
2018-04-27 06:34:22 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,17 +19,29 @@ client.on("message", function(message){
       name: client.user.username,
       icon_url: client.user.avatarURL
     },
-    title: "Milkyway Family",
+    title: "This is an embed",
     url: "http://google.com",
-    description: "this is our clans, choose the best one for you and join it now!",
-    
+    description: "This is a test embed to showcase what they look like and what they can do.",
+    fields: [{
+        name: "Fields",
+        value: "They can have different fields with small headlines."
+      },
+      {
+        name: "Masked links",
+        value: "You can put [masked links](http://google.com) inside of rich embeds."
+      },
+      {
+        name: "Markdown",
+        value: "You can put all the *usual* **__Markdown__** inside of them."
+      }
+    ],
+    timestamp: new Date(),
     footer: {
-      icon_url: client.author.avatarURL,
+      icon_url: client.user.avatarURL,
       text: "Bot By HBR"
     }
   }
 });
-
 
 		break;
 
