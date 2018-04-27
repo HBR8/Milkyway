6148909b17914dee373be2d2951cbe954d6ddc3c
HBR8 <38672560+HBR8@users.noreply.github.com>
2018-04-28 00:17:44 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,29 +3,6 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const PREFIX = "*";
 
-
-
-function loadJSON(success, error)
-{
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function()
-    {
-        if (xhr.readyState === XMLHttpRequest.DONE) {
-            if (xhr.status === 200) {
-                if (success)
-                    success(JSON.parse(xhr.responseText));
-            } else {
-                if (error)
-                    error(xhr);
-            }
-        }
-    };
-    xhr.open("GET", "https://api.royaleapi.com/clan/9VQ8V8YC","271845143019437687bd362749414e0db5d24accb17442f1aafe0aeb34d446a4",data, true);
-    xhr.send();
-}
-
-
-
 client.on("message", function(message){
   if(message.author.equals(client.user)) return;
   if(!message.content.startsWith(PREFIX)) return;
@@ -38,15 +15,6 @@ client.on("message", function(message){
     break;
 
     case "milkyway": 
-    
-    function clans(data) {
-      
-        var w = data;
-
-        message.channel.send(w.name);
-
-    }
-
 
     message.channel.send({embed: {
     color: 0xFFD700,
@@ -55,18 +23,31 @@ client.on("message", function(message){
       icon_url: client.user.avatarURL,
       url: "https://royaleapi.com/clan/family/milkyway/clans"
     },
-    description: "Our alliance is made up of 6 clans, you can join us!",
-    fields: [{
-        name: "Fields",
-        value: "They can have different fields with small headlines."
+    description: "* our alliance is made up of 6 clans with nearly 300 members. ",
+    fields: [
+      {
+        name: ":comet:  E A R T H **(#9VQ8V8YC)**",
+        value: ":medal: 40K+     :crossed_swords:Clan Wars"
       },
       {
-        name: "Masked links",
-        value: "You can put [masked links](http://google.com) inside of rich embeds."
+        name: ":comet:  N E P T U N E **(#P0JJ9RVJ)**",
+        value: ":medal: 32K+     :crossed_swords:Clan Wars"
       },
       {
-        name: "Markdown",
-        value: "You can put all the *usual* **__Markdown__** inside of them."
+        name: ":comet:  S A T U R N **(#P2P9VR02)**",
+        value: ":medal: 40K+     :crossed_swords:Clan Wars"
+      },
+      {
+        name: ":comet:  M A R S **(#9RURCVRR)**",
+        value: ":medal: 35K+     :crossed_swords:Clan Wars"
+      },
+      {
+        name: ":comet:  P L A N E T - X **(#99RP2Y2V)**",
+        value: ":medal: 37K+     :crossed_swords:Clan Wars"
+      },
+      {
+        name: ":comet:  J U P I T E R **(#P829Q822)**",
+        value: ":medal: 40K+     :crossed_swords:Clan Wars"
       }
     ],
     timestamp: new Date(),
