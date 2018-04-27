0f3cb35065e5bd3ece625f685426cf8b5b3f97f5
HBR8 <38672560+HBR8@users.noreply.github.com>
2018-04-28 00:57:20 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -56,7 +56,7 @@ client.on("message", function(message){
         url: "https://royaleapi.com/clan/family/milkyway/clan/P829Q822"
       }
     ],
-    setThumbnail: url: "https://royaleapi.com/static/img/badge/Twin_Peaks_02.png",
+    timestamp: new Date(),
     footer: {
       icon_url: client.user.avatarURL,
       text: "Bot By HBR"
