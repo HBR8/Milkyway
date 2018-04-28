eae89e63296d37f35cd7583d2141262194463391
HBR8 <38672560+HBR8@users.noreply.github.com>
2018-04-28 03:14:46 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -110,15 +110,59 @@ client.on("message", function(message){
     },
   }
 });
+    break;
+
+    case "Lol":
+    case "l":
+
+     message.channel.send({embed: {
+    color: 0xFFD700,
+    author: {
+      name: "E A R T H (#9VQ8V8YC)",
+      icon_url: "https://royaleapi.com/static/img/badge/Twin_Peaks_02.png",
+      url: "https://royaleapi.com/clan/family/milkyway/clan/9VQ8V8YC"
+    },
+    description: "welcome to Milkyway| Alliance of 6 competetive clans | Discord: milkyway.tk",
+    fields: [
+      {
+        name: "**Clan Score**",
+        value: ":trophy: 40K+",
+        inline: true
+      },
+      {
+        name: "**Required trophies**",
+        value: ":trophy: 4000",
+        inline: true
+      },
+      {
+        name: "**Donations Per Week**",
+        value: "13K+",
+        inline: true
+      },
+      {
+        name: "**Clan Wars**",
+        value: ":fire:[Active]",
+        inline: true
+      },
+      {
+        name: "**Region**",
+        value: "International",
+        inline: true
+      }
+    ],
 
 
-
-
+    timestamp:null ,
+    footer: {
+      icon_url: "https://cdn.discordapp.com/avatars/325523620032151553/d1dbfbd129157917cb2ef2bb878376ea.png?size=128",
+      text: "Bot By HBR"
+    },
+  }
+});
     break;
 
   case "NEPTUNE":
     case "NE":
-
      message.channel.send({embed: {
     color: 0xFFD700,
     author: {
@@ -163,10 +207,6 @@ client.on("message", function(message){
     },
   }
 });
-
-
-
-
     break;
 
     case "SATURN":
