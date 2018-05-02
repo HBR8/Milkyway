15af6d6cb8eb4808cd73e4282bd4f03bb6ed40c1
HBR8 <38672560+HBR8@users.noreply.github.com>
2018-05-02 17:09:13 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -51,7 +51,7 @@ client.on("message", function(message){
         url: "https://royaleapi.com/clan/family/milkyway/clan/P0JJ9RVJ"
       },
       {
-        name: "----> M E R C U R Y **(#P90VQC8R)** [**+12 Wins**]",
+        name: "----> M E R C U R Y **(#P90VQC8R)**",
         value: ":trophy: 1K+       :crossed_swords:Clan Wars[**Active**]",
         url: "https://royaleapi.com/clan/family/milkyway/clan/P90VQC8R"
       }
