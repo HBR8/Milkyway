0a8c191d3e309b6dfe2f1766b8e2d994bd25648b
HBR8 <38672560+HBR8@users.noreply.github.com>
2018-06-04 02:15:08 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -44,6 +44,11 @@ client.on("message", function(message){
         name: "----> N E P T U N E **(#P0JJ9RVJ)**",
         value: ":trophy: 31K+     :crossed_swords:Clan Wars[**Active**]",
         url: "https://royaleapi.com/clan/family/milkyway/clan/P0JJ9RVJ"
+      },
+      {
+        name: "----> M A R S **(#9RURCVRR)**",
+        value: ":trophy: 31K+     :crossed_swords:Clan Wars[**Active**]",
+        url: "https://royaleapi.com/clan/family/milkyway/clan/9RURCVRR"
       }
     ],
 
@@ -352,6 +357,56 @@ client.on("message", function(message){
 
 
 
+    break;
+
+
+    case "earth":
+    case "ea":
+
+     message.channel.send({embed: {
+    color: 0xFFD700,
+    author: {
+      name: "M A R S (#9RURCVRR)",
+      icon_url: "https://royaleapi.com/static/img/badge/Twin_Peaks_02.png",
+      url: "https://royaleapi.com/clan/family/milkyway/clan/9RURCVRR"
+    },
+    description: "Welcome to Milkyway #  Alliance of 6 competetive clans # Discord: milkyway.tk",
+    fields: [
+      {
+        name: "**Clan Score**",
+        value: ":trophy: 31K+",
+        inline: true
+      },
+      {
+        name: "**Required trophies**",
+        value: ":trophy: 2400",
+        inline: true
+      },
+      {
+        name: "**Donations Per Week**",
+        value: "11K+",
+        inline: true
+      },
+      {
+        name: "**Clan Wars**",
+        value: ":fire:[Active]",
+        inline: true
+      },
+      {
+        name: "**Region**",
+        value: "International",
+        inline: true
+      }
+    ],
+
+
+    timestamp:null ,
+    footer: {
+      icon_url: "https://cdn.discordapp.com/avatars/325523620032151553/d1dbfbd129157917cb2ef2bb878376ea.png?size=128",
+      text: "Bot By HBR"
+    },
+  }
+});
     break;
 
 
